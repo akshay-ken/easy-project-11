d0e0daaa65fdba63e73b514f5b6f2a98ff1ed803
akshay-ken <akshaybest3120@gmail.com>
2025-06-12 15:27:20 +0530

    practice task

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -3,7 +3,7 @@ export function CardImage() {
     <>
       <div className="md:w-1/2 relative">
         <div className="absolute top-0 left-0 w-full h-full bg-purple-800 opacity-60 mix-blend-multiply rounded-t-xl md:rounded-r-2xl md:rounded-t-none"></div>
-        <div className="h-full md:rounded-t-none rounded-t-xl md:rounded-r-2xl bg-cover bg-center bg-[url('../src/assets/images/image-header-mobile.jpg')] md:bg-[ulr('../src/assets/images/image-header-desktop.jpg')]">
+        <div className="h-full md:rounded-t-none rounded-t-xl md:rounded-r-2xl bg-cover bg-center bg-[url('../src/assets/images/image-header-mobile.jpg')] md:bg-[url('../src/assets/images/image-header-desktop.jpg')]">
           <div className="min-h-64 md:min-h-full"></div>
         </div>
       </div>
